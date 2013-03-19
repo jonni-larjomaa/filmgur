75972ff12b2749d47cc877af0d515c40441eb8a6
Jonni <mariox@mbnet.fi>
2013-03-19 21:07:08 +0200

    + fixed to work with actionbarsherlock!

--- Filmgur/src/foo/filmgur/LoginActivity.java
+++ Filmgur/src/foo/filmgur/LoginActivity.java
@@ -1,22 +1,16 @@
 package foo.filmgur;
 
+import com.actionbarsherlock.app.SherlockActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class LoginActivity extends Activity {
+
+public class LoginActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
+		getSupportActionBar().show();
 	}
 
 }
